d0796b33666b6e9e4be24ba59867c85a768a3c0a
jpiorun <jpiorun@unis.fr>
2016-02-04 13:41:54 +0100

    login controller

--- src/main/java/fil/iagl/idl/controller/UserController.java
+++ src/main/java/fil/iagl/idl/controller/UserController.java
@@ -1,13 +1,11 @@
 package fil.iagl.idl.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import fil.iagl.idl.FormulaireRecherche;
 import fil.iagl.idl.entite.User;
 import fil.iagl.idl.service.UserService;
 
@@ -22,15 +20,6 @@ public class UserController {
     public User create( @RequestBody User user ) {
         userService.create( user );
         return user;
-    }
-
-    @RequestMapping( value = "/search" , method = RequestMethod.POST )
-    public Boolean get( @ModelAttribute( "formulaireRecherche" ) final FormulaireRecherche formulaireRecherche) {
-
-        if ( formulaireRecherche.isCorrectlySet() ) {
-            return userService.get( formulaireRecherche.getLogin(), formulaireRecherche.getPassword() ) != null;
-        }
-        return false;
     }
 
 }
