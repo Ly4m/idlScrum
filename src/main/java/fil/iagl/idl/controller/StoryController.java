d5ee3712588278142cd3dff20593e3a4cc56dc9c
piorunj <jordan.piorun@gmail.com>
2016-02-06 21:22:58 +0100

    ajout du service/controller/mapper/dao pour les story

--- src/main/java/fil/iagl/idl/controller/StoryController.java
+++ src/main/java/fil/iagl/idl/controller/StoryController.java
@@ -1,19 +1,24 @@
 package fil.iagl.idl.controller;
 
-import fil.iagl.idl.service.StoryService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import fil.iagl.idl.entite.Story;
+import fil.iagl.idl.service.StoryService;
+
 @RestController
-@RequestMapping( "/story" )
+@RequestMapping("/story")
 public class StoryController {
 
-    @Autowired
-    private StoryService storyService;
+	@Autowired
+	private StoryService storyService;
 
-//    @RequestMapping( method = RequestMethod.GET , value = "/all" )
-//    public List< Story > getAll() {
-//
-//    }
+	@RequestMapping(method = RequestMethod.GET, value = "/all")
+	public List<Story> getAll() {
+		return storyService.getAll();
+	}
 }
---
+++ src/main/java/fil/iagl/idl/service/impl/StoryServiceImpl.java
@@ -0,0 +1,23 @@
+package fil.iagl.idl.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fil.iagl.idl.dao.StoryDao;
+import fil.iagl.idl.entite.Story;
+import fil.iagl.idl.service.StoryService;
+
+@Service
+public class StoryServiceImpl implements StoryService {
+
+	@Autowired
+	StoryDao storyDao;
+
+	@Override
+	public List<Story> getAll() {
+		return storyDao.getAll();
+	}
+
+}
